0e35d5f26349eda8ebf56b9be9ef63f8244cc6ad
Fabio Santos <ffsantos92@gmail.com>
2015-12-21 23:50:04 +0000

    Working on a webservice client

---
+++ webservice/app/WebServiceClient.php
@@ -0,0 +1,30 @@
+<?php
+
+/*
+ * This file is part of SmartGas, an iOS app to find the best gas station nearby.
+ *
+ * (c) Fábio Santos <ffsantos92@gmail.com>
+ * (c) Mateus Silva <mateusgsilva_@hotmail.com>
+ * (c) Fábio Marques <fabio1956.epo@gmail.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace App;
+
+class WebServiceClient extends \SoapClient
+{
+    private $username;
+    private $password;
+
+    public function __setUsernameTokenProfile($username, $password)
+    {
+        $this->username = $username;
+        $this->password = $password;
+    }
+
+    public function getHeader()
+    {
+    }
+}
