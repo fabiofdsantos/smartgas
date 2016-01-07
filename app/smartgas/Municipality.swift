d3ad042663bb29f77442724466cf14cd75500311
ffsantos92 <ffsantos92@gmail.com>
2016-01-07 22:55:31 +0000

    Add District and Municipality models; Update FuelStation and FuelType models

--- app/smartgas/Municipality.swift
+++ app/smartgas/Municipality.swift
@@ -1,42 +1,47 @@
 //
-//  FuelTypes.swift
+//  Municipality.swift
 //  smartgas
 //
-//  Created by Mateus Silva on 02/12/15.
-//  Copyright © 2015 Mateus Silva. All rights reserved.
+//  Created by Fábio Santos on 07/01/16.
+//  Copyright © 2016 Mateus Silva. All rights reserved.
 //
 
-import UIKit
+import Foundation
 
-class FuelType: NSObject, NSCoding {
+class Municipality: NSObject, NSCoding {
     var id: Int!
     var name: String!
+    var districtId: Int!
     
-    init(id: Int, name: String) {
+    init(id: Int, name: String, districtId: Int) {
         self.id = id
         self.name = name
+        self.districtId = districtId
     }
     
     struct Const {
         static let id = "id"
         static let name = "name"
+        static let districtId = "districtId"
     }
     
     // MARK: NSCoding
     func encodeWithCoder(coder: NSCoder) {
         coder.encodeInteger(id, forKey: Const.id)
         coder.encodeObject(name, forKey: Const.name)
+        coder.encodeInteger(districtId, forKey: Const.districtId)
     }
     
     required init?(coder decoder: NSCoder) {
         id = decoder.decodeIntegerForKey(Const.id)
         name = decoder.decodeObjectForKey(Const.name) as! String
+        districtId = decoder.decodeIntegerForKey(Const.districtId)
     }
     
-    static func saveMany (brands: [FuelType]) -> Bool {
+    static func saveMany (brands: [Municipality]) -> Bool {
         let documentsPath = NSURL(fileURLWithPath: NSSearchPathForDirectoriesInDomains(.DocumentDirectory, .UserDomainMask, true)[0])
         
-        let filePath = documentsPath.URLByAppendingPathComponent("FuelTypes.data")
+        let filePath = documentsPath.URLByAppendingPathComponent("Municipalities.data")
         
         let path = filePath.path!
         
@@ -47,17 +52,20 @@ class FuelType: NSObject, NSCoding {
         return false
     }
     
-    static func loadAll() -> [FuelType] {
-        var dataToRetrieve = [FuelType]()
+    static func loadAll() -> [Municipality] {
+        var dataToRetrieve = [Municipality]()
         
         let documentsPath = NSURL(fileURLWithPath: NSSearchPathForDirectoriesInDomains(.DocumentDirectory, .UserDomainMask, true)[0])
-        let filePath = documentsPath.URLByAppendingPathComponent("FuelTypes.data", isDirectory: false)
+        let filePath = documentsPath.URLByAppendingPathComponent("Municipalities.data", isDirectory: false)
         
         let path = filePath.path!
         
-        if let newData = NSKeyedUnarchiver.unarchiveObjectWithFile(path) as? [FuelType] {
+        if let newData = NSKeyedUnarchiver.unarchiveObjectWithFile(path) as? [Municipality] {
             dataToRetrieve = newData
         }
         return dataToRetrieve
     }
+
+    
+    
 }
